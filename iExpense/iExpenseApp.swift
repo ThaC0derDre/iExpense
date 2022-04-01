d206e9bf2f14cfb1df4b08519abb53c9ce98cd43
AndresGutierrez <c0dingw.dre@gmail.com>
2022-04-01 10:02:31 -0700

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Andres Gutierrez on 4/1/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
